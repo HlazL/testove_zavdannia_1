04c6170aab42a7ce7b126e0458e42293912c997f
HlazL <lena.glazkova21@gmail.com>
2022-01-14 19:47:57 +0200

    test_1

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def direction(facing, turn):
         return " Please provide correct data "
     elif (turn > 1080) or (turn < -1080) or (turn % 45 != 0):
         return " Please provide correct data "
-
-    angle = (turn / 45) % 8
-    new_direction = (directions.index(facing) + angle) % 8
+    else:
+        angle = (turn / 45) % 8
+        new_direction = (directions.index(facing) + angle) % 8
 
     return directions[int(new_direction)]
+
+direction("S", 180)
